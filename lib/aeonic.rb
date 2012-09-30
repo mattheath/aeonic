a5d76df07b541482994397a0661e9b20dbe7b3e3
Matt Heath <matt@mattheath.com>
2012-09-30 21:24:05 +0100

    Always parse with Chronic and return same output, convert timestamps prior to a format Chronic supports

--- lib/aeonic.rb
+++ lib/aeonic.rb
@@ -11,21 +11,19 @@ end
 class Aeonic
   def self.process(time)
 
-    if time.isTimestamp?
-      # If the time specified is a timestamp, output a human formatted date
-      puts self.convertTimestampToDate(time)
-    else
-      # Otherwise parse with chronic, and output the date and a unix timestamp
-      parsed = Chronic.parse(time)
-      puts "#{parsed} : " << (parsed.to_time.to_i.to_s)
-    end
+    # If the time specified is a timestamp, convert to a Time
+    time = self.convertTimestampToDate(time) if time.isTimestamp?
+
+    # Parse with chronic, and output the date and a unix timestamp
+    parsed = Chronic.parse(time)
+    puts "#{parsed} : " << (parsed.to_time.to_i.to_s)
 
   end
 
   private
 
   def self.convertTimestampToDate(time)
-    Time.strptime(time,'%s')
+    Time.strptime(time,'%s').to_s
   end
 
 end
